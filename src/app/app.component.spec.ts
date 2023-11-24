c81fffec4190f792eff6c209b82d5cc39aa3bb42
Juan Sanchez <js.sanchezl1@uniandes.edu.co>
2023-11-24 13:52:52 -0500

    base

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ISIS2603_202320_PARCIAL2'`, () => {
+  it(`should have as title 'series'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ISIS2603_202320_PARCIAL2');
+    expect(app.title).toEqual('series');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ISIS2603_202320_PARCIAL2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('series app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ISIS2603_202320_PARCIAL2';
+  title = 'series';
 }
